f7e8bde453f8c1112b50fb0793141cf1fb2a2ed2
PozzisMine <110555099+PozzisMine@users.noreply.github.com>
2026-06-05 19:11:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,73 @@
 const CSV_URL =
-  "https://docs.google.com/spreadsheets/u/0/d/1nlZSMyDzCxI8k6CkhG66B4uRjz7zrwT3VpCzJJPO7Dc/export?format=csv";
+"https://docs.google.com/spreadsheets/u/0/d/1nlZSMyDzCxI8k6CkhG66B4uRjz7zrwT3VpCzJJPO7Dc/export?format=csv";
 
 async function loadData() {
-  const response = await fetch(CSV_URL);
-  const csv = await response.text();
 
-  const rows = csv
-    .trim()
-    .split("\n")
-    .map(row => row.split(","));
+    const response = await fetch(CSV_URL);
+    const csv = await response.text();
 
-  return rows;
+    return csv
+        .trim()
+        .split("\n")
+        .map(row => row.split(","));
 }
 
 async function init() {
-  const rows = await loadData();
 
-  const last = rows[rows.length - 1];
+    const rows = await loadData();
 
-  document.getElementById("views").textContent =
-    Number(last[1]).toLocaleString();
+    const last = rows[rows.length - 1];
 
-  document.getElementById("likes").textContent =
-    Number(last[2]).toLocaleString();
+    document.getElementById("views").textContent =
+        Number(last[1]).toLocaleString();
 
-  document.getElementById("comments").textContent =
-    Number(last[3]).toLocaleString();
+    document.getElementById("likes").textContent =
+        Number(last[2]).toLocaleString();
+
+    document.getElementById("comments").textContent =
+        Number(last[3]).toLocaleString();
+
+    const labels = [];
+    const viewsData = [];
+
+    for(let i = 1; i < rows.length; i++) {
+
+        labels.push(rows[i][0]);
+
+        viewsData.push(
+            Number(rows[i][1])
+        );
+    }
+
+    const chart = echarts.init(
+        document.getElementById("viewsChart")
+    );
+
+    chart.setOption({
+
+        tooltip:{
+            trigger:"axis"
+        },
+
+        xAxis:{
+            type:"category",
+            data:labels
+        },
+
+        yAxis:{
+            type:"value"
+        },
+
+        series:[
+            {
+                name:"Просмотры",
+                type:"line",
+                smooth:true,
+                data:viewsData
+            }
+        ]
+    });
+
 }
 
 init();
